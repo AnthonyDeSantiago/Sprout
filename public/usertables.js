ba0ff19855663afa81d46d10442d9e952e9f544f
Camille Reaves <reaves.camille@gmail.com>
2023-09-24 23:34:03 -0400

    Update usertables.js

--- public/usertables.js
+++ public/usertables.js
@@ -21,13 +21,6 @@ const newUserRequest = collection(db, 'new_user_requests');
 const users = collection(db, 'users');
 //const auth = getAuth(app);
 
-/* changes the placeholder picture to whatever you picked in the Sign up Function */
-let profilePicture = document.getElementById("blank_choose_ur_pic");
-let inputFile = document.getElementById("input_file");
-inputFile.onchange = function(){
-    profilePicture.src = URL.createObjectURL(inputFile.files[0]);
-}
-
 console.log("usertables.js loaded")
 
 
