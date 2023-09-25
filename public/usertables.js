d7c30d4fdfb0f9be0e7ccab989545ae7202ae159
Camille Reaves <reaves.camille@gmail.com>
2023-09-25 19:57:26 -0400

    Update usertables.js
    typo # 3 :skull:

--- public/usertables.js
+++ public/usertables.js
@@ -128,7 +128,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         `;
     
         extendedTable.innerHTML = extendedTableHtml;
-        var table = document.getElementByClass(".extended-table");
+        var table = document.getElementByClassName(".extended-table");
         table.style.display = "contents";
     }
     
