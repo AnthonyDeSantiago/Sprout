539f0dac53f0fefd2d492fa6fa0b146e7322c599
jalon <jbailey1502@gmail.com>
2023-10-03 00:19:58 -0400

    suspend button update

--- public/js/admin_tables/admin_table_allusers.js
+++ public/js/admin_tables/admin_table_allusers.js
@@ -1,49 +1,61 @@
+// Log a message indicating that the script has been loaded
 console.log("!!! admin_table_allusers.js loaded !!!");
 
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
-import { collection, doc, getDoc, getDocs, addDoc, setDoc, updateDoc, Timestamp, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
-import { query, orderBy, limit, where, onSnapshot } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
-//import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js"
+// Import necessary functions from Firebase Firestore
+import { 
+    getFirestore, 
+    collection, 
+    doc, 
+    getDocs, 
+    updateDoc, 
+    query, 
+    where 
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
-//const auth = getAuth();
-
+// Get a reference to the Firestore database
 const db = getFirestore();
+
+// Get a reference to the 'users' collection in the Firestore database
 const users = collection(db, 'users');
 
-
-
-//--------------------------------------------------admin
+// Wait for the DOM content to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", async function () {
+    // Get references to various HTML elements
     const extendableTable = document.querySelector(".extendable-table");
     const extendedTable = document.querySelector(".extended-table");
+    const suspendButton = document.getElementById("suspend-button");
+    const suspendPopup = document.getElementById("suspendPopup");
+    const suspendConfirmButton = document.getElementById("suspend-confirm-button");
+    const suspendCancelButton = document.getElementById("suspend-cancel-button");
     const createUserButton = document.getElementById("createUserButton");
     const searchInput = document.getElementById("search");
 
+    // Event listener for 'Create User' button click
     createUserButton.addEventListener("click", () => {
         // Show the create user popup form
         document.getElementById("createUserPopup").style.display = "contents";
     });
 
-    // Add event listener for the create user form submission
-    const createUserForm = document.getElementById("create_user_form");
+    // Event listener for create user form submission
     createUserForm.addEventListener("submit", async function (e) {
         e.preventDefault();
 
-        // Get input values from the form
+        // Get the new username from the form input
         const newUsername = document.getElementById("newUsername").value;
-        // Get more input values as needed
+        // Implement logic to add the new user to Firebase Firestore here using 'newUsername'
 
-        // Add logic to create the new user using Firebase Firestore
-    
-        await addNewUserToFirestore(newUsername, /* other input values */);
-
-        // Close the popup after user creation
+        // Close the create user popup form after submission
         closeCreateUserPopup();
     });
+
+    // Function to filter users by username based on search input
     function filterUsersByUsername(username) {
+        // Get the table body element
         const tbody = document.querySelector(".extendable-table tbody");
+        // Get all rows in the table body
         const rows = tbody.querySelectorAll("tr");
+
+        // Loop through each row and check if the username matches the search term
         rows.forEach((row) => {
             const usernameCell = row.querySelector("td");
             const usernameText = usernameCell.textContent.toLowerCase();
@@ -55,183 +67,232 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
     }
 
-    // Add event listener for search input changes
+    // Event listener for search input changes
     searchInput.addEventListener("input", function () {
+        // Get the trimmed search term from the input
         const searchTerm = searchInput.value.trim();
+        // Call the filter function with the search term
         filterUsersByUsername(searchTerm);
     });
-});
 
-function addNewUserToFirestore(username, /* other input parameters */) {
-// Implement logic to add the new user to Firebase Firestore here
-}
+    // Function to add a new user to Firebase Firestore
+    async function addNewUserToFirestore(username) {
+        // Implement logic to add the new user to Firebase Firestore here using 'username'
+    }
 
-function closeCreateUserPopup() {
-// Close the create user popup form
-document.getElementById("createUserPopup").style.display = "none";
-}
-    // Function to populate the extendable table with user data
+    // Function to close the create user popup form
+    function closeCreateUserPopup() {
+        // Hide the create user popup form
+        document.getElementById("createUserPopup").style.display = "none";
+    }
+
+    // Function to load users from Firebase Firestore and populate the extendable table
     async function loadUsers() {
-        // Firebase data retrieval logic
-        // Loop through your users and create rows for each in the table
+        // Array to store retrieved users from Firestore
         var usersArray = [];
-        const q = query(users, where('role', '!=', "deleted")); //HERE IS WHERE WE COULD SET LIMITS IF WE WANTED TO PAGE THROUGH
-        const userDocs = await getDocs(q).then((querySnapshot) => {
-            var tempDoc = [];
-            querySnapshot.forEach((doc) => {
-                tempDoc.push({ id: doc.id, username: doc.get("username") })
-            });
-            usersArray = tempDoc;
+        // Query to get users where role is not 'deleted'
+        const q = query(users, where('role', '!=', "deleted"));
+        // Get the documents from the query
+        const userDocs = await getDocs(q);
+        // Loop through the documents and add user data to the usersArray
+        userDocs.forEach((doc) => {
+            usersArray.push({ id: doc.id, username: doc.get("username") });
         });
-        //const users = [{ username: "User1" }, { username: "User2" }, /* ... */];
 
+        // Get the table body element
         const tbody = extendableTable.querySelector("tbody");
-        tbody.innerHTML = ""; // Clear existing rows
+        // Clear existing rows in the table body
+        tbody.innerHTML = "";
 
+        // Loop through the usersArray and create rows for each user in the table
         usersArray.forEach((user) => {
             const row = document.createElement("tr");
             const usernameCell = document.createElement("td");
 
-            // Add a click event to the username cell
+            // Add the user's username to the username cell
             usernameCell.innerText = user.username;
+            // Add a click event listener to the username cell to show extended user data
             usernameCell.addEventListener("click", () => {
                 showExtendedTable(user.username);
             });
 
+            // Append the username cell to the row and the row to the table body
             row.appendChild(usernameCell);
             tbody.appendChild(row);
         });
     }
 
-    // Function to populate the extended table when a username is clicked
+    // Function to display extended user data when a username is clicked
     async function showExtendedTable(username) {
-        // Firebase data retrieval logic
-        // fetch data for the specific user by their username and populate the extended table with the unknown columns
+        // Array to store retrieved user data from Firestore
         var readUser = [];
-        username = username.toString();
+        // Query to get user data where username matches the clicked username
         const q = query(users, where('username', '==', username));
-        const getUsers = await getDocs(q).then((querySnapshot) => {
-            var tempDoc = [];
-            querySnapshot.forEach((doc) => {
-                tempDoc.push({ id: doc.id, ...doc.data() });
-            });
-            readUser = tempDoc;
+        // Get the documents from the query
+        const getUsers = await getDocs(q);
+        // Loop through the documents and add user data to the readUser array
+        getUsers.forEach((doc) => {
+            readUser.push({ id: doc.id, ...doc.data() });
         });
-    
-        // Get the user data object
+
+        // Get the user data object from the readUser array
         const userData = readUser[0];
         console.log(userData);
-    
+
+        // HTML content for the extended table displaying user details
         const extendedTableHtml = `
-            <table>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        <th>Approved</th>
-                        <th>Role</th>
-                        <th>Suspended</th>
-                        <th>E-mail</th>
-                        <th>Address</th>
-                        <th>DOB</th>
-                        <th>Password Last Created</th>
-                        <th>User Created</th>
-                        <th>User ID</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>${userData.firstName}</td>
-                        <td>${userData.lastName}</td>
-                        <td>${userData.username}</td>
-                        <td>${userData.approved}</td>
-                        <td>${userData.role}</td>
-                        <td>${userData.suspended}</td>
-                        <td>${userData.userEmail}</td>
-                        <td>${userData.address}</td>
-                        <td>${userData.DOB}</td>
-                        <td>${userData.passwordCreatedAt}</td>
-                        <td>${userData.userCreatedAt}</td>
-                        <td>${userData.id}</td>
-                    </tr>
-                </tbody>
-            </table>
-            
-            <!-- Buttons for Edit, Delete, Email, and Suspend -->
-            <div class="button-container">
-                <button id="edit-button" onclick="editUser('${userData.id}')">Edit User</button>
-                <button id="delete-button" onclick="confirmDelete('${userData.id}')">Delete</button>
-                <button id="email-button" onclick="emailUser('${userData.userEmail}')">Email User</button>
-                <button id="suspend-button" onclick="suspendUser('${userData.id}')">Suspend</button>
-            </div>
+            <!-- Extended table HTML content with user details -->
         `;
-    
+
+        // Set the inner HTML of the extended table with the generated content
         extendedTable.innerHTML = extendedTableHtml;
+        // Display the extended table
         document.getElementById("extended-table").style.display = "contents";
     }
+
+    // Function to suspend a user by updating their 'suspend' field to true
+    async function suspendUser(id) {
+        // Reference to the user document in Firestore using the provided ID
+        const userRef = doc(db, 'users', String(id));
+        // Update the 'suspend' field to true
+        await updateDoc(userRef, {
+            suspend: true
+        });
+    }
+
+    // Function to confirm and delete a user by updating their 'role' field to 'deleted'
+    async function confirmDelete(id) {
+        // Reference to the user document in Firestore using the provided ID
+        const userRef = doc(db, 'users', String(id));
+        // Update the 'role' field to 'deleted'
+        await updateDoc(userRef, {
+            role: "deleted"
+        });
+    }
+    let userIdToSuspend = null;
+
+    // Event listener for the 'Suspend' button click to display the suspend popup
+    suspendButton.addEventListener("click", (event) => {
+        // Get the user ID from the data attribute of the clicked element
+        userIdToSuspend = event.target.getAttribute("data-user-id");
     
+        // Show the suspend confirmation popup
+        document.getElementById("suspendPopup").style.display = "block";
+    
+        // Event listener for the 'Yes' button click in the suspend confirmation popup
+        suspendConfirmButton.addEventListener("click", () => {
+            // Close the suspend confirmation popup
+            document.getElementById("suspendPopup").style.display = "none";
+    
+            // Show the date/time selection popup
+            document.getElementById("datePopup").style.display = "block";
+        });
+    });
+    
+    // Event listener for the 'Submit' button click in the date/time selection popup
+    const dateSubmitButton = document.getElementById("date-submit-button");
+    dateSubmitButton.addEventListener("click", async () => {
+        // Get selected date and time values from the form inputs
+        const selectedDate = document.getElementById("date-input").value;
+        const selectedTime = document.getElementById("time-input").value;
+    
+        if (userIdToSuspend) {
+            // Reference to the user document in Firestore using the stored user ID
+            const userRef = doc(db, 'users', userIdToSuspend);
+    
+            try {
+                // Update the user's record in the Firestore database with the suspension date and time
+                await updateDoc(userRef, {
+                    suspend: true, // Set 'suspend' field to true to suspend the user
+                    suspensionDate: selectedDate,
+                    suspensionTime: selectedTime,
+                });
+    
+                // Display a confirmation message that the user has been suspended
+                alert("User has been suspended successfully.");
+            } catch (error) {
+                console.error("Error suspending user:", error);
+                alert("An error occurred while suspending the user. Please try again later.");
+            }
+    
+            // Reset the stored user ID after processing suspension
+            userIdToSuspend = null;
+        }
+    
+        // Close the date/time selection popup
+        document.getElementById("datePopup").style.display = "none";
+    });
+
+    // Event listener for the 'Edit' button click
+    const editButtons = document.querySelectorAll(".edit-button");
+    editButtons.forEach((button) => {
+        button.addEventListener("click", () => {
+            const userId = button.dataset.userId;
+            editUser(userId);
+        });
+    });
+
+    // Function to edit a user's information based on the provided user ID
+    async function editUser(userid) {
+        // Array to store retrieved user data from Firestore
+        var readUser = [];
+        // Convert the user ID to a string for query
+        const id = userid.toString();
+        // Query to get user data where 'id' field matches the provided user ID
+        const q = query(users, where('id', '==', id));
+        // Get the documents from the query
+        const getUsers = await getDocs(q);
+        // Loop through the documents and add user data to the readUser array
+        getUsers.forEach((doc) => {
+            readUser.push({ id: doc.id, ...doc.data() });
+        });
+
+        // Get the user data object from the readUser array
+        const userData = readUser[0];
+        
+        // Display the edit user popup form
+        document.getElementById("editUserPopup").style.display = "block";
+
+        // Event listener for edit user form submission
+        document.getElementById("editUserPopup").addEventListener("submit", async function (e) {
+            e.preventDefault();
+
+            // Get updated user information from the form inputs
+            const newUserName = document.getElementById("newUsername").value;
+            const newEmail = document.getElementById("newEmail").value;
+            const newFirstNameElement = document.getElementById("newfirst_name").value;
+            const newLastNameElement = document.getElementById("newlast_name").value;
+            const newAddressElement = document.getElementById("newaddress").value;
+            const newDOB = document.getElementById("newdateofbirth").value;
+
+            // Reference to the user document in Firestore using the provided user ID
+            const userRef = doc(db, 'users', String(id));
+
+            // Update user information in Firestore
+            await updateDoc(userRef, {
+                username: newUserName,
+                userEmail: newEmail,
+                firstName: newFirstNameElement,
+                lastName: newLastNameElement,
+                address: newAddressElement,
+                DOB: newDOB,
+            });
+
+            // Log a message indicating successful user update
+            console.log("User updated successfully");
+
+            // Close the edit user popup form after submission
+            document.getElementById("editUserPopup").style.display = "none";
+        });
+    }
+
+    const usernameCells = document.querySelectorAll(".extendable-table tbody td");
+    usernameCells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            showExtendedTable(cell.innerText);
+        });
+    });
+
     // Load user data when the page loads
     loadUsers();
 });
-
-async function suspendUser(id){
-    const userRef = doc(db, 'users', String(id));
-    
-    await updateDoc(userRef, {
-        suspend: true
-    });
-}
-
-async function confirmDelete(id){
-    const userRef = doc(db, 'users', String(id));
-    
-    await updateDoc(userRef, {
-        role: "deleted"
-    });
-}
-
-async function editUser(userid){
-    var readUser = [];
-    const id = userid.toString();
-    const q = query(users, where('id', '==', id));
-    const getUsers = await getDocs(q).then((querySnapshot) => {
-            var tempDoc = [];
-            querySnapshot.forEach((doc) => {
-                tempDoc.push({ id: doc.id, ...doc.data() });
-            });
-            readUser = tempDoc;
-    });
-    
-    // Get the user data object
-    const userData = readUser[0];
-    
-    document.getElementById("editUserPopup").style.display = "contents";
-    
-    document.getElementById("editUserPopup").addEventListener("submit", async function (e) {
-        e.preventDefault();
-        
-        const newUserName = document.getElementById("newUsername");
-        var newEmail = document.getElementById("newEmail");
-        const newfirstNameElement = document.getElementById("newfirst_name");
-        const newlastNameElement = document.getElementById("newlast_name");
-        const newaddressElement = document.getElementById("newaddress");
-        const newDOB = document.getElementById("newdateofbirth");
-
-        const userRef = doc(db, 'users', String(id));
-    
-        await updateDoc(userRef, {
-            username: newUserName,
-            userEmail: newEmail,
-            firstName: newfirstNameElement,
-            lastName: newlastNameElement,
-            address: newaddressElement,
-            DOB: newDOB,           
-        });
-
-        console.log("User updated successfully");
-    });
-
-}
-
-
