af807b7e77e39b22e1e3aad8cf519765f7a141c0
David <gkim51@students.kennesaw.edu>
2023-10-17 18:41:41 -0400

    ledger

--- Bootstrap/js/datatables-simple-demo.js
+++ Bootstrap/js/datatables-simple-demo.js
@@ -7,3 +7,56 @@ window.addEventListener('DOMContentLoaded', event => {
         new simpleDatatables.DataTable(datatablesSimple);
     }
 });
+
+new DataTable('#example', {
+    footerCallback: function (row, data, start, end, display) {
+        let api = this.api();
+
+        // Remove the formatting to get integer data for summation
+        let intVal = function (i) {
+            return typeof i === 'string'
+                ? i.replace(/[\$,]/g, '') * 1
+                : typeof i === 'number'
+                ? i
+                : 0;
+        };
+
+        // Total over all pages
+        total = api
+            .column(4)
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+
+        // Total over this page
+        pageTotal = api
+            .column(4, { page: 'current' })
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+
+        // Update footer
+        api.column(4).footer().innerHTML =
+            '$' + pageTotal + ' ( $' + total + ' total)';
+
+        
+            // Total over all pages
+        total = api
+            .column(3)
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+
+        // Total over this page
+        pageTotal = api
+            .column(3, { page: 'current' })
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+
+        // Update footer
+        api.column(3).footer().innerHTML =
+            '$' + pageTotal + ' ( $' + total + ' total)';
+
+    }
+});
+
+
+
+
