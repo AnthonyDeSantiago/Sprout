433da69bfdf1c17e4fec332ec08a2c79f95b2f26
AnthonyDeSantiago <94334680+AnthonyDeSantiago@users.noreply.github.com>
2023-10-18 19:19:11 -0400

    Added listener for Edit button

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -47,4 +47,40 @@ document.addEventListener("DOMContentLoaded", function () {
         accountForm.reset();
         location.reload();
     });
+
+    // Get all the checkbox elements
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Add an event listener to each checkbox
+    checkboxes.forEach(checkbox => {
+        console.log("dljslkfj");
+        checkbox.addEventListener('change', function () {
+            console.log("dljslkfj");
+            if (this.checked) {
+                // Get the data row identifier
+                const rowId = this.getAttribute('data-row');
+
+                // Find the related data element in the same row
+                const dataElement = document.querySelector(`td[data-row="${rowId}"]`);
+
+                // Access the data you need from the data element
+                const rowData = dataElement.textContent;
+
+                // Do something with the rowData
+                console.log('Checked:', rowData);
+            }
+        });
+    });
+
+    console.log("made it to the end");
+
 });
+
+const editButton = document.querySelector('.btn[data-bs-target="#editAccountModal"]');
+
+editButton.addEventListener('click', function() {
+  // This code will run when the Edit button is clicked
+  console.log('Edit button pressed');
+  // You can add your custom logic here
+});
+
