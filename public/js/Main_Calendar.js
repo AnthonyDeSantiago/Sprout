7de703453dc088bda3d26ffc37b12c28a4015539
David <gkim51@students.kennesaw.edu>
2023-10-27 16:04:34 -0400

    add ledger col. headings & date range filter

--- public/js/Main_Calendar.js
+++ public/js/Main_Calendar.js
@@ -98,3 +98,41 @@ button.onclick = () =>{
     but.style.opacity = "100";
 
 }  */
+
+
+
+/* Ledger date range filter */
+let minDate, maxDate;
+
+// Custom filtering function which will search data in column four between two values
+DataTable.ext.search.push(function (settings, data, dataIndex) {
+    let min = minDate.val();
+    let max = maxDate.val();
+    let date = new Date(data[1]);
+
+    if (
+        (min === null && max === null) ||
+        (min === null && date <= max) ||
+        (min <= date && max === null) ||
+        (min <= date && date <= max)
+    ) {
+        return true;
+    }
+    return false;
+});
+
+// Create date inputs
+minDate = new DateTime('#min', {
+    format: 'MMMM Do YYYY'
+});
+maxDate = new DateTime('#max', {
+    format: 'MMMM Do YYYY'
+});
+
+// DataTables initialisation
+let table = new DataTable('#ledger');
+
+// Refilter the table
+document.querySelectorAll('#min, #max').forEach((el) => {
+    el.addEventListener('change', () => table.draw());
+});
