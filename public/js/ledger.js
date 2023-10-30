381fbc1ddece48e7c5d9c4bf2f57c2f8aed6af56
AnthonyDeSantiago <94334680+AnthonyDeSantiago@users.noreply.github.com>
2023-10-30 14:56:05 -0400

    ledger.js grabs transactions of the account

--- public/js/ledger.js
+++ public/js/ledger.js
@@ -1,4 +1,4 @@
-import { getAccountID } from "./database_module.mjs";
+import { getAccountID, getDocsWithValue, getFieldValue } from "./database_module.mjs";
 
 console.log("ledger.js has loaded!!!");
 
@@ -6,10 +6,14 @@ const urlParameters = new URLSearchParams(window.location.search);
 const accountNumber = urlParameters.get("accountNumber");
 const accountName = urlParameters.get("accountName");
 const accountID = await getAccountID(accountNumber);
+const journalEntries = await getDocsWithValue('journals', 'account', accountID);
+const transactions = await getDocsWithValue('transactions', 'account', accountID);
 
 
 console.log("accountNumber:", accountNumber);
 console.log("accountName:", accountName);
 console.log("account ID:", accountID);
+console.log("journal entries:", journalEntries);
+console.log("transactions:", transactions);
 
 
