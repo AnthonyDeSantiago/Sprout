879c359038c8643c62e97037c77c8dbe7a043413
AnthonyDeSantiago <94334680+AnthonyDeSantiago@users.noreply.github.com>
2023-10-30 23:18:07 -0400

    journal_entry is now pulling entry info from db

--- public/js/journal_entry.js
+++ public/js/journal_entry.js
@@ -1,1 +1,33 @@
+import { getDocumentReference, getFieldValue } from "./database_module.mjs";
+
 console.log("journal_entry.js has loaded");
+
+const urlParameters = new URLSearchParams(window.location.search);
+const journal = urlParameters.get("journal");
+const transactions = await getFieldValue('journals', journal, 'transactions');
+
+console.log("journal id: ", journal);
+console.log("transactions: ", transactions);
+console.log("test transaction: ", await getDocumentReference('transactions', transactions[1]));
+
+populateTable(transactions, 'journalEntry_table');
+$(document).ready(function() {
+    console.log("Is this working now?");
+    $('#journalEntry_table').DataTable();
+});
+
+async function populateTable(transactions, tableId) {
+    const tableBody = document.querySelector(`#${tableId} tbody`);
+    for (let i = 0; i < transactions.length; i++) {
+      const transaction = await getDocumentReference('transactions', transactions[i]);
+      tableBody.innerHTML += `
+            <tr>
+                <td>${transaction.account}</td>
+                <td>${transaction.user}</td>
+                <td>${transaction.description}</td>
+                <td>${transaction.debit}</td>
+                <td>${transaction.credit}</td>
+            </tr>
+        `;
+    }
+}
