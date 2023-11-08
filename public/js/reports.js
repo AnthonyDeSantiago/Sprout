c1bd5b6486ae6cea45d705f984402c6d70ad0075
jalon <jbailey1502@gmail.com>
2023-11-07 19:58:40 -0500

    reports

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,19 +1,85 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add event listener for Save button
-    document.getElementById('saveReport').addEventListener('click', function() {
-        saveReport();
-    });
+  // Add event listener for Generate Reports button
+  document.getElementById('dateRangeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Get the selected dates
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+    const currentDate = new Date().toISOString().split('T')[0]; // Format as 'YYYY-MM-DD'
 
-    // Add event listener for Email button
-    document.getElementById('emailReport').addEventListener('click', function() {
-        emailReport();
-    });
+    // Validate the date range
+    if (new Date(startDate) > new Date(endDate)) {
+        alert('The start date must be before the end date.');
+        return;
+    }
 
-    // Add event listener for Print button
-    document.getElementById('printReport').addEventListener('click', function() {
-        printReport();
-    });
+    // Ensure neither date is in the future
+    if (startDate > currentDate || endDate > currentDate) {
+        alert('You cannot select a future date.');
+        return;
+    }
+
+    // Generate reports based on the selected date range
+    generateReports(startDate, endDate);
 });
+
+
+    function generateReports(startDate, endDate) {
+        // Now, you need to pass the start and end dates to the fetchDataAndRenderTable function
+        fetchDataAndRenderTable('trialBalanceTable', 'trial_balance', trialBalanceColumns, startDate, endDate);
+        fetchDataAndRenderTable('incomeStatementTable', 'income_statement', incomeStatementColumns, startDate, endDate);
+        fetchDataAndRenderTable('balanceSheetTable', 'balance_sheet', balanceSheetColumns, startDate, endDate);
+        fetchDataAndRenderTable('retainedEarningsTable', 'retained_earnings', retainedEarningsColumns, startDate, endDate);
+    }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Fetch and render tables when the page is ready
+    fetchDataAndRenderTables();
+
+    // Add event listener for Generate Reports button
+    document.getElementById('dateRangeForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+        generateReports();
+    });
+
+    // Add event listeners for action buttons
+    document.getElementById('saveReport').addEventListener('click', function() {
+        performReportAction('save');
+    });
+
+    document.getElementById('emailReport').addEventListener('click', function() {
+        performReportAction('email');
+    });
+
+    document.getElementById('printReport').addEventListener('click', function() {
+        performReportAction('print');
+    });
+    
+});
+
+
+
+
+
+function performReportAction(actionType) {
+    const selectedReport = document.getElementById('reportSelector').value;
+    const reportData = getReportData(selectedReport);
+
+    switch(actionType) {
+        case 'save':
+            saveReport(reportData);
+            break;
+        case 'email':
+            emailReport(reportData);
+            break;
+        case 'print':
+            printReport(reportData);
+            break;
+        default:
+            console.error('Invalid action type');
+    }
+}
 
 // Function to handle the Save Report operation
 function saveReport() {
@@ -80,15 +146,75 @@ function printReport() {
 }
 
 // Placeholder function to get report data
-// In reality, this would be pulling data from your application's state or DOM
-function getReportData() {
-    // This is just a placeholder - it would be your actual report data
-    return {
-        title: 'Report Title',
-        dateRange: {
-            start: document.getElementById('startDate').value,
-            end: document.getElementById('endDate').value,
-        },
-        // ...all other report data
-    };
+function getReportData(selectedReport) {
+    // Depending on the report selected, fetch the appropriate data
+    if (selectedReport === 'all') {
+        return {
+            trialBalance: $('#trialBalanceTable').DataTable().data().toArray(),
+            incomeStatement: $('#incomeStatementTable').DataTable().data().toArray(),
+            balanceSheet: $('#balanceSheetTable').DataTable().data().toArray(),
+            retainedEarnings: $('#retainedEarningsTable').DataTable().data().toArray(),
+        };
+    } else {
+        return $(`#${selectedReport}Table`).DataTable().data().toArray();
+    }
 }
+
+function fetchDataAndRenderTable(tableId, firebaseRef, columns, startDate, endDate) {
+    var table = $('#' + tableId).DataTable({
+        columns: columns,
+        searching: false,
+        paging: false
+    });
+
+    // Assuming 'date' is the field in your Firebase data that you want to filter by
+    var query = database.ref(firebaseRef).orderByChild('date');
+    if(startDate) {
+        query = query.startAt(startDate);
+    }
+    if(endDate) {
+        query = query.endAt(endDate);
+    }
+
+    // Fetch data from Firebase with the new query
+    query.on('value', function(snapshot) {
+        table.clear();
+        snapshot.forEach(function(childSnapshot) {
+            var childData = childSnapshot.val();
+            var rowData = columns.map(column => childData[column.data] || '');
+            table.row.add(rowData);
+        });
+        table.draw();
+    });
+}
+
+  // Define columns for each table
+  var trialBalanceColumns = [
+    { title: 'Accounts', data: 'account' },
+    { title: 'Debit Balances', data: 'debit' },
+    { title: 'Credit Balances', data: 'credit' }
+  ];
+  var incomeStatementColumns = [
+    { title: 'Revenue Streams', data: 'revenue' },
+    { title: 'Expenses', data: 'expense' },
+    { title: 'Net Income', data: 'netIncome' }
+  ];
+  var balanceSheetColumns = [
+    { title: 'Assets', data: 'asset' },
+    { title: 'Liabilities', data: 'liability' },
+    { title: 'Equity', data: 'equity' }
+  ];
+  var retainedEarningsColumns = [
+    { title: 'Beginning Retained Earnings', data: 'beginning' },
+    { title: 'Net Income', data: 'netIncome' },
+    { title: 'Dividends', data: 'dividend' },
+    { title: 'Ending Retained Earnings', data: 'ending' }
+  ];
+  
+  // Call function to render each table
+  $(document).ready(function() {
+    fetchDataAndRenderTable('trialBalanceTable', 'trial_balance', trialBalanceColumns);
+    fetchDataAndRenderTable('incomeStatementTable', 'income_statement', incomeStatementColumns);
+    fetchDataAndRenderTable('balanceSheetTable', 'balance_sheet', balanceSheetColumns);
+    fetchDataAndRenderTable('retainedEarningsTable', 'retained_earnings', retainedEarningsColumns);
+  });
