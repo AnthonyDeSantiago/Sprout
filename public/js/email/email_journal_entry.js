261081def1ff2cc67e7919c1e3e767b1752970ae
David <gkim51@students.kennesaw.edu>
2023-11-10 15:29:28 -0500

    make btn and temp for journal entry and adj entry email

---
+++ public/js/email/email_journal_entry.js
@@ -0,0 +1,38 @@
+const button = document.querySelector('.journal-btn');
+
+button.onclick = () => {
+        
+        var templateParams = {
+            name: document.getElementById('name').value,
+        };
+    
+        emailjs.send('service_9bu3nfr', 'template_fqcnfto', templateParams)
+        .then(
+            res => {
+                document.getElementById("name").value = "",
+                console.log.apply(res)
+            alert("message sent sucessfully");
+        })
+        .catch((err) => console.log(err));
+    
+}
+
+
+const button2 = document.querySelector('.adjust-btn');
+
+button2.onclick = () => {
+        
+        var templateParams = {
+            name: document.getElementById('name2').value,
+        };
+    
+        emailjs.send('service_9bu3nfr', 'template_e9qtgt3', templateParams)
+        .then(
+            res => {
+                document.getElementById("name2").value = "",
+                console.log.apply(res)
+            alert("message sent sucessfully");
+        })
+        .catch((err) => console.log(err));
+    
+}
